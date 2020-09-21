c16327b974bb6dcfc0c2cd4ad2a5cb376dd6d3ae
amr-raazi <50888029+amr-raazi@users.noreply.github.com>
2020-09-21 22:52:44 +0530

    Updated script. More use specific now.

--- add text to properties.py
+++ add text to properties.py
@@ -4,22 +4,44 @@ import pytesseract
 from PIL import Image, ImageFilter
 import stat
 import re
+import glob
+
+
+def split(list_of_strings, split_on_num):
+    out = []
+    for element in list_of_strings:
+        while len(element) > split_on_num:
+            out.append(element[:split_on_num])
+            element = element[split_on_num:]
+        if len(element) > 0:
+            out.append(element)
+    return out
+
+
+def trim_list(list_of_strings, max_length):
+    out = []
+    for i in list_of_strings:
+        if len(i) > max_length:
+            i = i[:(len(i) // max_length) * max_length]
+        out.append(i)
+    return out
+
 
 # setting Path
 pytesseract.pytesseract.tesseract_cmd = r'D:\Coding\ImgPy\Tesseract\tesseract.exe'
 
 # user variables
-folder_path = r"D:\Coding\ImgPy\photos"
+folder_path = r""
 blur = False
 grayscale = False
 text_to_find = ''
-file_list = os.listdir(folder_path)
+
 os.chdir(folder_path)
+file_list = glob.glob('*.jpg')
 for image_path in file_list:
-    # store exif data
+    print(image_path)
     exif_dict = piexif.load(image_path)
     comment_asci = []
-    # open and process file for ocr
     img = Image.open(image_path)
     if grayscale:
         img = img.convert("L")
@@ -29,39 +51,38 @@ for image_path in file_list:
     img.save(image_path)
     # ocr
     ocr_text = pytesseract.image_to_string(Image.open(image_path), lang='eng', config="-c tessedit_char_whitelist"
-                                                                                      "=ITEMNO.itemno.1234567890")
+                                                                                      "=NO.no1234567890()")
     # process text given by ocr
-    # remove last bad char
+    ocr_text = ocr_text.lower()
     ocr_text = ocr_text[:-1]
-    # remove spaces
     ocr_text = ocr_text.replace(" ", "")
-    # strip leading and trailing blanks
     ocr_text = ocr_text.strip()
-    # split lines into list
     ocr_text = ocr_text.splitlines()
     comment = ''
     # iterate through lines
     for line in ocr_text:
         # check if text_to_find is in the line
         if text_to_find in line:
-            # add from text_to_find's first letter
             index = line.index(text_to_find[0])
+            line = re.sub(r'\([^)]*\)', '', line)
             line = line[index:]
-            # add line from text_to_find's first letter in line
-            comment += line + ","
-    # remove some chars using regex
-    comment = re.sub('[a-zA-Z .]', '', comment)
+            comment += line + "\n"
+    comment = re.sub('[()a-zA-Z.]', '', comment)
+    comment = comment.strip()
+    comment = comment.splitlines()
+    comment = split(comment, 6)
+    comment = trim_list(comment, 6)
+    comment = "\n".join(comment)
     print(comment)
     # convert into ascii
-    for i in comment:
-        asci = ord(i)
-        comment_asci.append(asci)
+    for char in comment:
+        ascii_char = ord(char)
+        comment_asci.append(ascii_char)
         comment_asci.append(0)
     comment_asci.append(0)
     comment_asci.append(0)
-    # convert into tuple
     comment_asci = tuple(comment_asci)
     # set it as property in file
-    exif_dict['0th'][piexif.ImageIFD.XPKeywords] = comment_asci
+    exif_dict['0th'][piexif.ImageIFD.XPComment] = comment_asci
     exif_bytes = piexif.dump(exif_dict)
     piexif.insert(exif_bytes, image_path)
